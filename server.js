6935aed396ee06cfe9e156b9d4ab808432968ae8
kwamkert <tomriddle477@gmail.com>
2020-04-25 08:55:31 +0000

    View documents updated

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ res.sendFile(path.join(__dirname+'/dist/ndbank-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+const port = process.env.PORT || 8080;
+console.log("Listening on ",port)
